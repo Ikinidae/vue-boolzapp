4a9f33dbcb0f148c13fc6cb2b3cf6e34e452a4af
Ikinidae <federicaelia24@gmail.com>
2022-05-29 12:12:53 +0200

    Circa conclusa colonna a sx con elenco chat

---
+++ script.js
@@ -0,0 +1,171 @@
+const chats = new Vue ({
+    el : "#boolzapp",
+    data : {
+        contacts : [
+            {
+                name : "Stefano",
+                avatar : "img/index.png",
+                visible : true,
+                messages : [
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Yooooo",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Come va?",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Oii tutto bene tu?",
+                        status : "received"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Tutto bene!",
+                        status : "sent"
+                    }
+                ] 
+            },
+            {
+                name : "Stefano",
+                avatar : "img/index.png",
+                visible : true,
+                messages : [
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Yooooo",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Come va?",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Oii tutto bene tu?",
+                        status : "received"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Tutto bene!",
+                        status : "sent"
+                    }
+                ] 
+            },
+            {
+                name : "Stefano",
+                avatar : "img/index.png",
+                visible : true,
+                messages : [
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Yooooo",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Come va?",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Oii tutto bene tu?",
+                        status : "received"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Tutto bene!",
+                        status : "sent"
+                    }
+                ] 
+            },
+            {
+                name : "Stefano",
+                avatar : "img/index.png",
+                visible : true,
+                messages : [
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Yooooo",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Come va?",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Oii tutto bene tu?",
+                        status : "received"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Tutto bene!",
+                        status : "sent"
+                    }
+                ] 
+            },
+            {
+                name : "Stefano",
+                avatar : "img/index.png",
+                visible : true,
+                messages : [
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Yooooo",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Come va?",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Oii tutto bene tu?",
+                        status : "received"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Tutto bene!",
+                        status : "sent"
+                    }
+                ] 
+            },
+            {
+                name : "Stefano",
+                avatar : "img/index.png",
+                visible : true,
+                messages : [
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Yooooo",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Come va?",
+                        status : "sent"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Oii tutto bene tu?",
+                        status : "received"
+                    },
+                    {
+                        date : "31/05/2022 15:30:42",
+                        message : "Tutto bene!",
+                        status : "sent"
+                    }
+                ] 
+            }
+        ]
+    }
+
+    // console.log(chats.contacts.name);
+});
